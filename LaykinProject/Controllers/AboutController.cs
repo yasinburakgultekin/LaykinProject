6ab252a1f5f475a4d0d0846189fa0be7ab55b4e1
Yasin Burak Gültekin <yasinburakgultekin@hotmail.com>
2020-04-08 17:45:56 +0300

    Projenin son hali eklendi.

---
+++ LaykinProject/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaykinProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Laykin Danışmanlık Ltd. Şti.";
+            return View();
+        }
+    }
+}
